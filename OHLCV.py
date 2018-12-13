a7c9d8885d7ed0328136883d73abc40b21f357aa
Didier SERVAIS <servais.didier@gmail.com>
2018-12-13 01:12:37 +0100

    Create OHLCV.py

---
+++ OHLCV.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import time
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root + '/python')
+
+import ccxt  # noqa: E402
+
+# -----------------------------------------------------------------------------
+# common constants
+
+msec = 1000
+minute = 60 * msec
+hold = 30
+pair ='ETH/EUR'
+
+# -----------------------------------------------------------------------------
+
+exchange = ccxt.gdax({
+    'rateLimit': 3000,
+    'enableRateLimit': True,
+    # 'verbose': True,
+})
+
+# -----------------------------------------------------------------------------
+
+from_datetime = '2018-12-13 00:00:00'
+from_timestamp = exchange.parse8601(from_datetime)
+
+# -----------------------------------------------------------------------------
+
+now = exchange.milliseconds()
+
+# -----------------------------------------------------------------------------
+
+data = []
+
+while from_timestamp < now:
+
+    try:
+
+        print(exchange.milliseconds(), 'Fetching candles starting from', exchange.iso8601(from_timestamp))
+        ohlcvs = exchange.fetch_ohlcv(pair, '1m', from_timestamp)
+        print(exchange.milliseconds(), 'Fetched', len(ohlcvs), 'candles')
+        from_timestamp = ohlcvs[-1][0]
+        data += ohlcvs
+        print (data)
+
+    except (ccxt.ExchangeError, ccxt.AuthenticationError, ccxt.ExchangeNotAvailable, ccxt.RequestTimeout) as error:
+
+        print('Got an error', type(error).__name__, error.args, ', retrying in', hold, 'seconds...')
+        time.sleep(hold)
